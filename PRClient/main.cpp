46a5a82f39783a79356374181cc58fd8fa3eb361
rocik <rocik@rocik-pc.localdomain>
2017-02-25 13:56:37 +0100

    Creation of Client application.

---
+++ PRClient/main.cpp
@@ -0,0 +1,22 @@
+/*
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: rocik
+ *
+ * Created on February 25, 2017, 1:53 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+	return 0;
+}
+
