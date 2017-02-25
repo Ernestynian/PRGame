97e96d6b001d7ca3ea2164069a5b5a37b4c23723
Rocik <rrocik@vp.pl>
2017-02-25 15:35:12 +0100

    SDL setup.

--- PRClient/main.cpp
+++ PRClient/main.cpp
@@ -1,22 +1,21 @@
-/*
- */
+#include <SDL2/SDL.h>
 
-/* 
- * File:   main.cpp
- * Author: rocik
- *
- * Created on February 25, 2017, 1:53 PM
- */
-
-#include <cstdlib>
-
-using namespace std;
-
-/*
- * 
- */
-int main(int argc, char** argv) {
-
+int main(int argc, char* argv[]) {
+	SDL_Window* window(0);
+	
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		SDL_Quit();
+		
+		return -1;
+	}
+	
+	window = SDL_CreateWindow("SDL 2.0 Test",
+				SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+				800, 600, SDL_WINDOW_SHOWN);
+	
+	SDL_Delay(3000);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	
 	return 0;
 }
-
